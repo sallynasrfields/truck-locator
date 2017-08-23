6c1cf71c6e75c850cc67cf2dfdfb3f8a7c64e9ac
Marie Dagan <mdagan03@gmail.com>
2017-08-22 21:10:19 -0500

    added code for account profile page

--- app/components/Account.js
+++ app/components/Account.js
@@ -12,8 +12,154 @@ render(){
     return(  
       <div>
         <AccountHeader setChild={this.setChild.bind(this)}/>
-
-        ...This is the account  page     
+<div className="row">
+        <div className="col-lg-12">
+          <div className="panel panel-default vendor-log-in">
+            <div className="panel-body">
+              <form>
+                <div className="form-group">
+                  <label htmlFor="inputName">Name</label>
+                  <input type="text" className="form-control" id="inputName" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="inputCompanyName">Company Name</label>
+                  <input type="text" className="form-control" id="inputCompanyName" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="inputEmail">Email</label>
+                  <input type="email" className="form-control" id="inputEmail" />
+                </div>
+                <p>Reset Password</p>
+                <div className="form-group">
+                  <label htmlFor="newPassword">New Password</label>
+                  <input type="password" className="form-control" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="confirmPassword">Confirm Password</label>
+                  <input type="password" className="form-control" />
+                </div>
+                <div>
+                  <label className="control-label">Upload Photo</label>
+                  <input id="input-1" type="file" className="file" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="website">Website</label>
+                  <input type="text" className="form-control" id="website" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="comment">Describe Your Food Truck:</label>
+                  <textarea className="form-control" rows={5} id="comment" defaultValue={""} />
+                </div>
+                <section className="container">
+                  <div className="dropdown">
+                    <select name="one" className="dropdown-select">
+                      <option value>Display Yelp Ratings?</option>
+                      <option value={1}>Yes</option>
+                      <option value={2}>No</option>
+                    </select>
+                  </div>
+                </section>
+                <br />
+                <p>Type of Cuisine
+                </p><section className="container">
+                  <div className="form-check-label">
+                    <label className="form-check-label">
+                      <input type="checkbox" className="form=check-input" />
+                      Mexican
+                    </label>
+                    <label className="form-check-label">
+                      <input type="checkbox" className="form=check-input" />
+                      Italian
+                    </label>
+                    <label className="form-check-label">
+                      <input type="checkbox" className="form=check-input" />
+                      Indian
+                    </label>
+                    <label className="form-check-label">
+                      <input type="checkbox" className="form=check-input" />
+                      Cajun
+                    </label>
+                    <label className="form-check-label">
+                      <input type="checkbox" className="form=check-input" />
+                      Soul
+                    </label>
+                    <label className="form-check-label">
+                      <input type="checkbox" className="form=check-input" />
+                      Thai
+                    </label>
+                    <label className="form-check-label">
+                      <input type="checkbox" className="form=check-input" />
+                      Greek
+                    </label>
+                    <label className="form-check-label">
+                      <input type="checkbox" className="form=check-input" />
+                      Chinese
+                    </label>
+                    <label className="form-check-label">
+                      <input type="checkbox" className="form=check-input" />
+                      Japanese
+                    </label>
+                    <label className="form-check-label">
+                      <input type="checkbox" className="form=check-input" />
+                      American
+                    </label>
+                    <label className="form-check-label">
+                      <input type="checkbox" className="form=check-input" />
+                      Vietnamese
+                    </label>
+                    <label className="form-check-label">
+                      <input type="checkbox" className="form=check-input" />
+                      Mediterranean
+                    </label>
+                    <label className="form-check-label">
+                      <input type="checkbox" className="form=check-input" />
+                      Korean
+                    </label>
+                    <label className="form-check-label">
+                      <input type="checkbox" className="form=check-input" />
+                      Caribbean
+                    </label>
+                    <label className="form-check-label">
+                      <input type="checkbox" className="form=check-input" />
+                      Lebanese
+                    </label>
+                    <label className="form-check-label">
+                      <input type="checkbox" className="form=check-input" />
+                      Moroccan
+                    </label>
+                    <label className="form-check-label">
+                      <input type="checkbox" className="form=check-input" />
+                      French
+                    </label>
+                    <label className="form-check-label">
+                      <input type="checkbox" className="form=check-input" />
+                      Spanish
+                    </label>
+                    <label className="form-check-label">
+                      <input type="checkbox" className="form=check-input" />
+                      German
+                    </label>
+                    <label className="form-check-label">
+                      <input type="checkbox" className="form=check-input" />
+                      Turkish
+                    </label>
+                    <label className="form-check-label">
+                      <input type="checkbox" className="form=check-input" />
+                      Vegetarian
+                    </label>
+                    <label className="form-check-label">
+                      <input type="checkbox" className="form=check-input" />
+                      Vegan
+                    </label>
+                  </div>
+                </section>
+                <button type="submit" className="btn btn-primary">Submit</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+     
        
       </div>
     );
